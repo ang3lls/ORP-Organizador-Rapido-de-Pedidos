6d42018548ff21694fa17cc298d060d96e80141e
eeikee <konumaeike5618@hotmail.com>
2020-05-01 22:30:14 -0300

    Adicionado os novos icones e cores do app

--- MobileApp/app/src/main/java/com/rdt/orp/Load_Logo.java
+++ MobileApp/app/src/main/java/com/rdt/orp/Load_Logo.java
@@ -2,7 +2,10 @@ package com.rdt.orp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class Load_Logo extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Load_Logo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load__logo);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager. LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+                    public void run(){
+                        startActivity(new Intent(getBaseContext(), MainActivity.class));
+                        finish();
+            }
+
+            },3000);
+        }
     }
-}
