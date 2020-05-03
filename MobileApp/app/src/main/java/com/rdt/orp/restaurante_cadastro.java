abc2fa8e26387a986a6a1ea3bf02fdb4d99b75d9
ang3lls <angelina.barboza@hotmail.com.br>
2020-05-02 23:27:55 -0300

    pagina de cadastro do restaurante

---
+++ MobileApp/app/src/main/java/com/rdt/orp/restaurante_cadastro.java
@@ -0,0 +1,14 @@
+package com.rdt.orp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class restaurante_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurante_cadastro);
+    }
+}
